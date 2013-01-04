2e0718ea55905e722a1d8d237afc08321d0e8390
kenjiskywalker <git@kenjiskywalker.org>
2013-01-05 00:49:50 +0900

    sample codes

---
+++ lib/fluent-plugin-hoge.rb
@@ -0,0 +1,9 @@
+require "fluent-plugin-hoge/version"
+
+module Fluent
+  module Plugin
+    module Hoge
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent-plugin-hoge/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Hoge
+      VERSION = "0.0.1"
+    end
+  end
+end
